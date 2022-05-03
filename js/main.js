a65e1c413c96cfd99bba1c6547c7cf58bbdbd5d0
ColtFleming <fle20001@byui.edu>
2022-05-03 16:10:21 -0600

    Commited changes to the index, js, and css pages

--- js/main.js
+++ js/main.js
@@ -1,29 +1,35 @@
 const links = [
   {
     label: "Week 1",
-    url: "week1/story_editor.html"
+    url: "week1/story_editor.html",
+    class: "one"
   },
   {
     label:"Week 2",
-    url: "week2/index.html"
+    url: "week2/index.html",
+    class: "two"
   },
   {
     label:"Week 3",
-    url:"week3/index.html"
+    url:"week3/index.html",
+    class: "three"
   }
 ];
 
-let jsfile = document.getElementById('jsfile');
-let ul = document.createElement('ul');
-for(let i = 0, max = links.length; i < max; i++){
-  let li = document.createElement('li');
-  let a = document.createElement('a');
-  let link = document.createTextNode(links[i].label)
-  a.appendChild(link);
-  a.title = links[i].label;
-  a.href = links[i].url;
-  li.appendChild(a);
-  ul.appendChild(li);
+function generateList(){
+  let jsfile = document.getElementById('jsfile');
+  let ul = document.createElement('ul');
+  for(let i = 0, max = links.length; i < max; i++){
+    let a = document.createElement('a');
+    a.setAttribute('class', links[i].class);
+    let li = document.createElement('li');
+    li.setAttribute('id', 'listItems');
+    let link = document.createTextNode(links[i].label)
+    a.appendChild(link);
+    a.title = links[i].label;
+    a.href = links[i].url;
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
+  jsfile.appendChild(ul);
 }
-jsfile.appendChild(ul);
-
