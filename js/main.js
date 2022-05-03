5b9936c5820d706a95a9ce1e474ea1c11a0a388d
Colt Fleming <coltfleming@gmail.com>
2022-05-02 20:01:56 -0600

    Adding code to js and to css changing some info on the index

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 const links = [
   {
-    label: "Week1 Story Editor",
+    label: "Week 1",
     url: "week1/index.html"
+  },
+  {
+    label:"Week 2",
+    url: "week2/index.html"
+  },
+  {
+    label:"Week 3",
+    url:"week3/index.html"
   }
 ]
+
+let text = '<ul>'
+for(let i = 0, max = links.length; i < max; i++){
+  text = text + '<li><a href="' + links[i].url +'">' + links[i].label + '</a></li>';
+}
+text = text + '</ul>';
+console.log(text);
+document.getElementById("jsfile").innerHTML = text;
