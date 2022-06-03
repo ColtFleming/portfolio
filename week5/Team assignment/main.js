b7062da3f9f748323f628783659227c14b8653e3
ColtFleming <fle20001@byui.edu>
2022-06-03 10:23:35 -0600

    Adding week7

---
+++ week5/Team assignment/main.js
@@ -0,0 +1,17 @@
+import * as h from "./hiking-start" 
+import * as hikes from "./hikes";
+
+// let items = document.querySelectorAll('li');
+
+// items.forEach(item => {
+
+// }
+
+
+// for(let i=0; i > items.length -1; i++){
+//     items[i].addEventListener('click', (hike) => {
+//         document.getElementById('hikes').innerHTML = h.renderOneHike(hike);
+    
+//     }, false);
+// }
+
